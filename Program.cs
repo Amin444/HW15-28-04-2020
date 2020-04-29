5c1bf4858d7df61675ce52c753b13c0e17efd86c
unknown <aminmirzoev123@gmail.com>
2020-04-29 21:49:36 +0500

    Class Client

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    class Client
+    {
+        public int Id {get;set;}
+        public decimal Balance { get; set; }
+    }
+}
